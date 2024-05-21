e2c74f7dc1a30697da6ede78cb3eec71c318dfc1
Charlotte Roy <roychrltt@chacha.home>
2024-05-21 18:13:55 +0200

    utils finis

--- get_next_line.c
+++ get_next_line.c
@@ -1,6 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xiaxu <xiaxu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 12:26:00 by xiaxu             #+#    #+#             */
+/*   Updated: 2024/05/21 13:21:37 by xiaxu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
 
 char	*get_next_line(int fd)
 {
+	int			i;
+	char		*readed;
+	static char	*rem;
 
+	if (!get_rem(&rem, fd))
+		reutrn (NULL);
+	return (readed);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xiaxu <xiaxu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/21 13:22:51 by xiaxu             #+#    #+#             */
+/*   Updated: 2024/05/21 18:13:29 by xiaxu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 size_t	ft_strlen(const char *s)
@@ -10,36 +22,59 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-static size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+size_t	ft_strchr(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (i);
+}
+
+static void	ft_strcpy(char *dst, const char *src)
 {
 	size_t	i;
 
-	if (!dstsize)
-		return (ft_strlen(src));
 	i = 0;
-	while (src[i] && i < dstsize - 1)
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = 0;
-	return (ft_strlen(src));
+	while (src[i])
+		dst[i] = src[i++];
 }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	size_t	len1;
 	size_t	len2;
-	char	*res;
+	char	*result;
 
 	if (!s1 || !s2)
 		return (NULL);
 	len1 = ft_strlen(s1);
 	len2 = ft_strlen(s2);
-	res = malloc(len1 + len2 + 1);
-	if (!res)
+	result = malloc(len1 + len2 + 1);
+	if (!result)
 		return (NULL);
-	ft_strlcpy(res, s1, len1 + 1);
-	ft_strlcpy((res + len1), s2, len2 + 1);
-	return (res);
+	ft_strcpy(res, s1);
+	ft_strcpy((res + len1), s2);
+	result[len1 + len2] = 0;
+	return (result);
+}
+
+char	*ft_substr(char const *s, unsigned int start)
+{
+	char	*sub;
+	size_t	i;
+
+	i = ft_strlen(s + start + 1);
+	sub = malloc(i + 1);
+	if (!sub)
+		return (NULL);
+	sub[i] = 0;
+	while (i--)
+		sub[i] = s[start + i + 1];
+	return (sub);
 }
