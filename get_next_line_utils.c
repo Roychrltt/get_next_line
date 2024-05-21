26fedb55438097c7872f190f699d7bf5ce799631
Charlotte Roy <roychrltt@chacha.home>
2024-05-21 08:38:14 +0200

    move from home

---
+++ get_next_line_utils.c
@@ -0,0 +1,45 @@
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+static size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	if (!dstsize)
+		return (ft_strlen(src));
+	i = 0;
+	while (src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	return (ft_strlen(src));
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*res;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	res = malloc(len1 + len2 + 1);
+	if (!res)
+		return (NULL);
+	ft_strlcpy(res, s1, len1 + 1);
+	ft_strlcpy((res + len1), s2, len2 + 1);
+	return (res);
+}
